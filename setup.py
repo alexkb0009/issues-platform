6f1acb5826c752cf769b5ada5863ff2523f3277a
Alex <alex@ics.com>
2015-02-14 21:01:40 -0500

    Test Commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='My Issues',
+      version='0.1',
+      description='Issues Web Application for BAC Project',
+      author='Alexander Balashov',
+      author_email='alex.balashov@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
