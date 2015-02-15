6f4f54846975fdb577ff17d50e348805b43d0dae
Alex <alex@ics.com>
2015-02-14 23:08:46 -0500

    Test PyMongo again

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='My Issues',
       author='Alexander Balashov',
       author_email='alex.balashov@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['wsgi-request-logger>=0.4', 'passlib>=1.6', 'Jinja2>=2.7', 'pymongo>=2.8']
+      install_requires=['wsgi-request-logger>=0.4', 'passlib>=1.6', 'Jinja2>=2.7', 'pymongo']
 #      install_requires=['Bottle>=0.13'],
      )
